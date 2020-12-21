25b7f3c45936388a0796f326e019e86bed953698
Max Yasynchuk <nora22022002@gamil.com>
2020-12-21 21:01:15 +0200

    hello world #1

---
+++ modules/custom/kp_main/src/Controller/FirstPage.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ * @title
+ * Containe \Drupal\kp_main\Controller\FirstPage.
+ */
+
+namespace Drupal\kp_main\Controller;
+
+/**
+ * Provides route for our costum module. 
+ */
+class FirstPage{
+	
+	/**
+	* Display simple page.
+	*/
+	public function content() {
+		return array(
+			'@markup' => 'Hello world',
+		);
+}
+}
